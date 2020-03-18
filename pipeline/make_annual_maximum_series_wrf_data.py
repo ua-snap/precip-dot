11d88257f5d4d637a5bc79064ba0795c1c6d298d
Cameron Tauxe <ctauxe@neptuneinc.org>
2020-03-18 20:26:59 +0000

    split ams by decade for projected data

--- pipeline/make_annual_maximum_series_wrf_data.py
+++ pipeline/make_annual_maximum_series_wrf_data.py
@@ -5,20 +5,32 @@
 def make_ams( ds ):
     return ds.resample(time='Y').max().compute()
 
-def run( x ):
-    fn, out_fn = x
-    ds = xr.open_mfdataset( fn, combine='by_coords' )
+def run(duration):
+
+    # build args
+    file_names = sorted(glob.glob(os.path.join(path,'*_{}_*{}*.nc').format(duration, data_group)))
+    #out_files = [os.path.join(out_path,'pcpt_{}_sum_wrf_{}_ams.nc'.format(data_group, duration)) for duration in DURATIONS_NOAA]
+
+    ds = xr.open_mfdataset(file_names, combine='by_coords' )
     ds_ams = make_ams( ds )
     
     # write it back out to disk with compression encoding
     encoding = ds_ams[ 'pcpt' ].encoding
     encoding.update( zlib=True, complevel=5, contiguous=False, chunksizes=None, dtype='float32' )
     ds_ams[ 'pcpt' ].encoding = encoding
-    
-    ds_ams.to_netcdf( out_fn )
+
+    timeslices = [('1979','2015')]
+    # For rcp85 data (2006-2100), we split up results by decade
+    if ('rcp85' in data_group):
+        timeslices = [(str(x),str(x+9)) for x in range(2020,2100,10)]
+
+    for ts in timeslices:
+        ds_slice = ds_ams.sel(time=slice(ts[0],ts[1]))
+        out_fn = os.path.join(out_path,'pcpt_{}_sum_wrf_{}_{}-{}_ams.nc'.format(data_group, duration, ts[0], ts[1]))
+        ds_slice.to_netcdf(out_fn)
+
     ds.close(); ds = None
     ds_ams.close(); ds_ams = None
-    return out_fn
 
 if __name__ == '__main__':
     import os, glob, itertools
@@ -40,34 +52,8 @@ if __name__ == '__main__':
     data_group = args.data_group
     # ncpus = args.ncpus
 
-    # # # # # testing
-    # path = '/workspace/Shared/Tech_Projects/DOT/project_data/wrf_pcpt/durations'
-    # out_path = '/workspace/Shared/Tech_Projects/DOT/project_data/wrf_pcpt/annual_maximum_series'
-    # data_group = 'NCAR-CCSM4_historical'
-    # # ncpus = 32
-    # # # # end testing
-
     DURATIONS_NOAA = ['60m','2h','3h','6h','12h','24h','2d','3d','4d','7d','10d','20d','30d','45d','60d',] # names of the durations in the NOAA tool
-    
-    # build args
-    file_groups = [sorted(glob.glob(os.path.join(path,'*_{}_*{}*.nc').format(duration, data_group))) for duration in DURATIONS_NOAA]
-    out_files = [os.path.join(out_path,'pcpt_{}_sum_wrf_{}_ams.nc'.format(data_group, duration)) for duration in DURATIONS_NOAA]
-    args = list(zip(file_groups,out_files))
 
-    # run in serial
-    out = [] 
-    for x in args:
-        out = out + [run(x)]
-
-    # # run in parallel [non-working due to some RAM process overheads]
-    # ncpus = 2
-    # try:
-    #   pool = mp.Pool( ncpus )
-    #   out = pool.map( run, args )
-    #   pool.close()
-    #   pool.join()
-    # except:
-    #   # make sure the pool is closed
-    #   pool.close()
-    #   pool.join()
-    #   pool = None
+    for d in DURATIONS_NOAA:
+        print(" duration: {}".format(d))
+        run(d)

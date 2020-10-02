3063f47a5c769c99a7dbb005998b0f0a16bfc708
kyleredilla <kmredilla@alaska.edu>
2020-10-02 09:59:36 -0800

    use env var; check for output files before work

--- other_scripts/atlas14_data_access_preprocessing/project_grid_to_akalbers_noaa_atlas14_data.py
+++ other_scripts/atlas14_data_access_preprocessing/project_grid_to_akalbers_noaa_atlas14_data.py
@@ -1,13 +1,15 @@
-# # # # # # # # # # # # # # # # # # # # # # # # # 
-# # NOAA Atlas 14 data -- reproject to EPSG:3338
-# # # # # # # # # # # # # # # # # # # # # # # # #
+# reproject extracted Atlas 14 data to epsg:3338
 
-def nad83_to_3338( fn, bounds ):
-    print(fn)
+import os, glob, subprocess
+import multiprocessing as mp
 
-    dirname, basename = os.path.split(fn)
 
-    output_path = dirname.replace('extracted', 'warped')
+def nad83_to_3338(fp, bounds):
+    print(fp)
+
+    dirname, basename = os.path.split(fp)
+
+    output_path = dirname.replace("extracted", "warped")
     try:
         if not os.path.exists(output_path):
             _ = os.makedirs(output_path)
@@ -15,36 +17,67 @@ def nad83_to_3338( fn, bounds ):
         pass
 
     del_files = []
-    out_fn = os.path.join(output_path, basename.replace('.asc', '.tif'))
-    _ = subprocess.call(['gdalwarp', '-q', '-overwrite', '-srcnodata', '-9','-dstnodata', '-9999', '-s_srs', 'NAD83', '-t_srs', 'EPSG:3338', '-co', 'COMPRESS=LZW', '-te'] + bounds + [fn, out_fn ])
+    out_fp = os.path.join(output_path, basename.replace(".asc", ".tif"))
+    _ = subprocess.call(
+        [
+            "gdalwarp",
+            "-q",
+            "-overwrite",
+            "-srcnodata",
+            "-9",
+            "-dstnodata",
+            "-9999",
+            "-s_srs",
+            "NAD83",
+            "-t_srs",
+            "EPSG:3338",
+            "-co",
+            "COMPRESS=LZW",
+            "-te",
+        ]
+        + bounds
+        + [fp, out_fp]
+    )
 
-    return out_fn
+    return out_fp
 
-def run( x ):
+
+def run(x):
     return nad83_to_3338(*x)
 
-if __name__ == '__main__':
-    import os, glob, subprocess, fiona, copy
-    import multiprocessing as mp
 
-    path = '/workspace/Shared/Tech_Projects/DOT/project_data/NOAA_Atlas14/raw/extracted'
-    shp_fn = '/workspace/Shared/Tech_Projects/DOT/project_data/NOAA_Atlas14/shapefiles/AK_Extent_3338.shp'
-    
-    # with fiona.open( shp_fn ) as shp:
-    #     bounds = [str(i) for i in shp.bounds] # to pass to gdalwarp
+if __name__ == "__main__":
+    # list the data
+    down_dir = os.getenv("A14_DOWNLOAD_DIR")
+    raw_dir = os.path.dirname(down_dir)
+    extr_dir = os.path.join(raw_dir, "extracted")
+    extr_fps = glob.glob(os.path.join(extr_dir, "*.asc"))
 
-    # hardwire the bounds since I buffered the above by 10k meters on each side to make sure we get it all.
-    bounds = ['-2176652.08815013', '405257.4902562425', '1501904.8634676873', '2384357.9149669986']
+    # only need to reproject if not present in warped
+    warp_dir = os.path.join(raw_dir, "warped")
+    warp_fns = [os.path.basename(fn) for fn in glob.glob(os.path.join(warp_dir, "*"))]
+    extr_fps = [
+        fp
+        for fp in extr_fps
+        if os.path.basename(fp).replace(".asc", ".tif") not in warp_fns
+    ]
+    # make warped dir if not present
+    if not os.path.isdir(warp_dir):
+        os.mkdir(warp_dir)
 
-    # list the files:
-    files = glob.glob(os.path.join(path, '*.asc'))
+    # hardwire the bounds of AK extent (3338)
+    bounds = [
+        "-2176652.08815013",
+        "405257.4902562425",
+        "1501904.8634676873",
+        "2384357.9149669986",
+    ]
 
     # args
-    args = [ (fn,bounds) for fn in files ]
+    args = [(fp, bounds) for fp in extr_fps]
 
     # parallel run
     pool = mp.Pool(30)
-    out = pool.map( run, args )
+    out = pool.map(run, args)
     pool.close()
     pool.join()
-
